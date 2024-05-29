8ff7d7d3bdb4c2e9ea7c6f73a6edf060b3fb81df
Ome Mishra <omemishra@users.noreply.github.com>
2024-05-30 02:44:54 +0530

    Update Redacted-Request.py
    JSON support

--- Redacted-Request.py
+++ Redacted-Request.py
@@ -4,10 +4,11 @@ from java.awt.datatransfer import StringSelection
 from java.awt import Toolkit
 from java.awt.event import ActionListener
 import re
+import json
 
 # Welcome message
-print("Welcome to the Redacted Request Extension v1.0 by OME MISHRA!")
-print("Copyright (c) 2023 OME MISHRA. All rights reserved.")
+print("Welcome to the Redacted Request Extension v1.1 by OME MISHRA!")
+print("Copyright (c) 2024 OME MISHRA. All rights reserved.")
 print("This code is protected by copyright law. Unauthorized copying or distribution is prohibited.")
 
 class BurpExtender(IBurpExtender, IContextMenuFactory):
@@ -40,20 +41,45 @@ class BurpExtender(IBurpExtender, IContextMenuFactory):
         def actionPerformed(self, e):
             request = self._selectedMessage.getRequest()
             requestBytes = self._helpers.bytesToString(request)
+    
+            # Define sensitive information to redact
+            sensitive_info = ['Cookie', 'Authorization', 'X-Amz-Security-Token', 'Email', 'Username', 'Password', 'Token', 'Key', 'Apikey']
+    
+            # Redact sensitive information
+            modifiedRequestBytes = requestBytes
+            for info in sensitive_info:
             
-            # Redact the Cookie header
-            modifiedRequestBytes = re.sub(r'Cookie: .*', 'Cookie: Redacted', requestBytes)
-            
-            # Redact the Authorization header
-            modifiedRequestBytes = re.sub(r'Authorization: .*', 'Authorization: Redacted', modifiedRequestBytes)
-            
-            # Redact the X-Amz-Security-Token header
-            modifiedRequestBytes = re.sub(r'X-Amz-Security-Token: .*', 'X-Amz-Security-Token: Redacted', modifiedRequestBytes)
-            
+                # With Headers
+                modifiedRequestBytes = re.sub(info + r': .*', info + ': Redacted', modifiedRequestBytes)
+                
+                #Body Plain
+                modifiedRequestBytes = re.sub(info.lower() + r'=.*', info.lower() + '=Redacted', modifiedRequestBytes)
+                
+            # With JSON
+            try:
+                # Split the request body 
+                header_end_index = modifiedRequestBytes.index('\r\n\r\n') + 4
+                header_section = modifiedRequestBytes[:header_end_index]
+                body_section = modifiedRequestBytes[header_end_index:]
+
+                # Attempt to parse the body 
+                json_data = json.loads(body_section)
+
+                # Redact sensitive information 
+                for key in json_data:
+                    if key.lower() in [info.lower() for info in sensitive_info]:
+                        json_data[key] = 'Redacted'
+
+                # Combine headers and redacted JSON data
+                modifiedRequestBytes = header_section + json.dumps(json_data)
+            except ValueError:
+                pass  # Ignore if the request body is not JSON or cannot be parsed
+
+    
             stringSelection = StringSelection(modifiedRequestBytes)
             clipboard = Toolkit.getDefaultToolkit().getSystemClipboard()
             clipboard.setContents(stringSelection, None)
-            print("Request copied to clipboard.")
+            print("Request copied to clipboard.")   
 
 if __name__ in ["__main__", "burp"]:
     BurpExtender()
